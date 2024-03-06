771fc5915797bc233bf293f7086937b9e1dd38fd
federico-dibitonto <f.dibitonto94@gmail.com>
2024-03-06 12:54:38 +0100

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: 'Helloworld.vue'
+}
+</script>
+
+<template>
+    <div class="color">
+        <h1 class="text-center">ciao</h1>
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
