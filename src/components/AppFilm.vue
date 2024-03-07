88d8df8506481ec311e195fabed214345081e19f
federico-dibitonto <f.dibitonto94@gmail.com>
2024-03-07 13:12:03 +0100

    milestone 3 completed

--- src/components/AppFilm.vue
+++ src/components/AppFilm.vue
@@ -15,6 +15,18 @@ export default {
         },
         movieFlag(currentElement){
                 return `https://flagcdn.com/16x12/${currentElement}.png`;
+        },
+        rating(currentElement){
+            let ratingStar = Math.round(currentElement / 2);
+            let emptyStar = Array(5).fill('fa-solid fa-star');
+            
+            // Use v-bind:class instead of modifying the style directly
+            return emptyStar.map((starClass, index) => ({
+                class: {
+                [starClass]: index < ratingStar, // Apply class based on filled stars
+                'text-danger': index < ratingStar, // Apply color for filled stars
+                },
+            }));
         }
     }
 }
@@ -24,9 +36,10 @@ export default {
     <h2 class="ms-2 mb-3">FILM</h2>
     <div class="d-flex flex-wrap mb-5">
         <div class="my_grid m-auto" v-for="currentElement in store.popularFilm">
-            <img class="img-fluid" :src="movieImg(currentElement.poster_path)" alt="">
+            <img class="my_img-fluid" :src="movieImg(currentElement.poster_path)" alt="">
             <div>{{ currentElement.title }}</div>
             <div><img :src="movieFlag(currentElement.original_language)" alt=""></div>
+            <div><i v-for="(star, index) in rating(currentElement.vote_average)" :key="index"><i v-bind:class="star.class"></i></i></div>
         </div>
     </div>
 </template>
--- src/components/AppSeries.vue
+++ src/components/AppSeries.vue
@@ -15,6 +15,18 @@ export default {
         },
         seriesFlag(currentElement){
                 return `https://flagcdn.com/16x12/${currentElement}.png`;
+        },
+        rating(currentElement){
+            let ratingStar = Math.round(currentElement / 2);
+            let emptyStar = Array(5).fill('fa-solid fa-star');
+            
+            // Use v-bind:class instead of modifying the style directly
+            return emptyStar.map((starClass, index) => ({
+                class: {
+                [starClass]: index < ratingStar, // Apply class based on filled stars
+                'text-danger': index < ratingStar, // Apply color for filled stars
+                },
+            }));
         }
     }
 }
@@ -24,9 +36,10 @@ export default {
     <h2 class="ms-2 mb-3">SERIE TV</h2>
     <div class="d-flex flex-wrap">
         <div class="my_grid m-auto" v-for="currentElement in store.popularSeries">
-            <img  class="img-fluid" :src="seriesImg(currentElement.poster_path)" alt="">
+            <img  class="my_img-fluid" :src="seriesImg(currentElement.poster_path)" alt="">
             <div>{{ currentElement.name }}</div>
             <div><img :src="seriesFlag(currentElement.original_language)" alt=""></div>
+            <div><i v-for="(star, index) in rating(currentElement.vote_average)" :key="index"><i v-bind:class="star.class"></i></i></div>
         </div>
     </div>
 </template>
